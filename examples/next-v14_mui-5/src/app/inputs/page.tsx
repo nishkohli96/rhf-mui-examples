a814fce19e315cc31ade57965661ba0ff5d28377
Nishant <nishkohli96@gmail.com>
2025-04-30 04:43:33 +0530

    next-v14-mui-5 setup

---
+++ examples/next-v14_mui-5/src/app/inputs/page.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from 'next';
+import dynamic from 'next/dynamic';
+import { ContentContainer, LinksList, PageHeading, SubHeading } from '@/components';
+import { DocsLinks, SourceCodeLinks, CodeSandboxLinks } from '@/constants';
+
+const title = 'Inputs';
+const description = 'Form utilizing RHFTextField, RHFPasswordInput, RHFNumberInput, RHFTagsInput and RHFFileUploader with validation managed via react-hook-form\'s register options.';
+
+const ClientForm = dynamic(() => import('@/forms/inputs-with-register-options'), { ssr: false });
+
+export const metadata: Metadata = {
+  title,
+  description
+};
+
+const TextFieldPage = () => {
+  const links = [
+    DocsLinks.rhfTextField,
+    DocsLinks.rhfNumberInput,
+    DocsLinks.rhfPasswordInput,
+    DocsLinks.rhfTagsInput,
+    DocsLinks.rhfFileUploader
+  ];
+  const codeLinks = [
+    SourceCodeLinks.inputs,
+    CodeSandboxLinks.inputs
+  ];
+
+  return (
+    <ContentContainer>
+      <PageHeading title={title} />
+      <SubHeading title={description}/>
+      <ClientForm />
+      <LinksList links={links} />
+      <LinksList links={codeLinks} areCodeLinks />
+    </ContentContainer>
+  );
+};
+
+export default TextFieldPage;
---
+++ examples/next-v14_mui-5/src/app/layout.tsx
@@ -0,0 +1,57 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import Grid from '@mui/material/Grid2';
+import { ToastContainer } from 'react-toastify';
+import { AppThemeProvider } from '@/theme';
+import { AppBar, Drawer, Footer } from '@/components';
+import './globals.css';
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+const inter = Inter({ subsets: ['latin'] });
+const defaultTitle = 'RHF-Mui Components';
+
+export const metadata: Metadata = {
+  title: {
+    template: `%s | ${defaultTitle}`,
+    default: defaultTitle
+  },
+  description: 'Examples for RHF-Mui Components'
+};
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AppRouterCacheProvider options={{ key: 'mui' }}>
+          <AppThemeProvider>
+            <AppBar />
+            <Grid container className="content">
+              <Grid
+                size={{ md: 3 }}
+                sx={{ display: { xs: 'none', md: 'block' } }}
+              >
+                <Drawer />
+              </Grid>
+              <Grid size={{ xs: 12, md: 9 }}>
+                {children}
+              </Grid>
+            </Grid>
+            <Footer />
+            <ToastContainer
+              autoClose={3000}
+              limit={1}
+              closeButton
+              style={{ fontSize: '1rem' }}
+            />
+          </AppThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
